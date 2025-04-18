8200b3d97f1768b6fed8becba4f15b581c341c0b
KOUR0SH2007 <koroshlesani@gmail.com>
2025-04-19 00:14:24 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('V2Ray server is running!');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
